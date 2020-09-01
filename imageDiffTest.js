463a1976fb127e40f3d1c9640c2b2c2197e3b1c9
cortisiko <curtis.david7@gmail.com>
2020-09-01 12:27:08 -0400

    cleaning up

--- imageDiffTest.js
+++ imageDiffTest.js
@@ -1,19 +1,31 @@
 import Rembrandt from 'rembrandt'
 import fs from "fs"
 
- let firstImage, secondImage, rembrandt
+ let baseImage, secondImage, rembrandt
 
- firstImage ='images/HaroldQApassed.png'
- secondImage = 'images/HaroldQApassed.png',
+ baseImage ='images/HaroldQApassed.png'
+ secondImage = 'images/HaroldQApassedincorrect.png',
 
-  rembrandt = new Rembrandt({
-    imageA: firstImage,
+   rembrandt = new Rembrandt({
+    imageA: baseImage,
     imageB: secondImage,
     renderComposition: true,
     compositionMaskColor: Rembrandt.Color.RED, // Color of unmatched pixels
     maxOffset: 0,
     maxThreshold: 0
 
+});
+
+let getResults = ((result)=>{
+  console.log('Result is:', result.passed)
+  console.log('Pixel Difference:', result.differences, `Percentage Difference: ${result.percentageDifference.toFixed(5)}%`)
+
+});
+
+let writeImageDiffToFile = ((result)=>{
+  getResults(result)
+  let buff = result.compositionImage
+  fs.writeFileSync(`diffImages/imageDiff${Date.now()}.png`, buff);
 })
 
  
@@ -21,11 +33,12 @@ import fs from "fs"
 rembrandt.compare()
   .then(function (result) {
 
-    console.log('Passed:', result.passed)
-    console.log('Pixel Difference:', result.differences, 'Percentage Difference', result.percentageDifference, '%')
+    result == 'true' ? getResults(result) : writeImageDiffToFile(result);
+    // console.log('Passed:', result.passed)
+    // console.log('Pixel Difference:', result.differences, 'Percentage Difference', result.percentageDifference, '%')
  
-    let buff = result.compositionImage
-    fs.writeFileSync(`diffImages/test${Date.now()}.png`, buff);
+    // let buff = result.compositionImage
+    // fs.writeFileSync(`diffImages/test${Date.now()}.png`, buff);
     
 })
   .catch((e) => {
